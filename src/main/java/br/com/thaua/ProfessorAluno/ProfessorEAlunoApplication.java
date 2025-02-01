5440fb17bc5f8f4455efe6bdc07b1e9b5ef62eb5
Thauã <tagashi.dev@gmail.com>
2025-02-01 14:47:51 -0300

    First commit

---
+++ src/main/java/br/com/thaua/ProfessorAluno/ProfessorEAlunoApplication.java
@@ -0,0 +1,13 @@
+package br.com.thaua.ProfessorAluno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorEAlunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorEAlunoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/thaua/ProfessorAluno/ProfessorEAlunoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thaua.ProfessorAluno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorEAlunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
